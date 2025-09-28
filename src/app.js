a669f6a7c416460e88d1d707b8017fea91425408
VinokyrovAP <madnessstalkerl@gmail.com>
2025-09-28 23:56:03 +0500

    #chapter5: add Dispatcher and funct createApp

---
+++ src/app.js
@@ -0,0 +1,51 @@
+const { destroyDOM } = require("./destroy-dom");
+const { Dispatcher } = require("./dispatcher");
+const { mountDOM } = require("./mount-dom");
+
+const createApp = ({ state, view, reducers = {} }) => {
+  let parentEl = null;
+  let vdom = null;
+
+  const dispatcher = new Dispatcher();
+  const subscritions = [dispatcher.afterEveryCommand(renderApp)];
+
+  const emit = (eventName, payload) => {
+    dispatcher.dispatch(eventName, payload);
+  };
+
+  for (const actionName in reducers) {
+    const reducer = reducers[actionName];
+
+    const subs = dispatcher.subscribe(actionName, (payload) => {
+      state = reducer(state, payload);
+    });
+
+    subscritions.push(subs);
+  }
+
+  function renderApp() {
+    if (vdom) {
+      destroyDOM(vdom);
+    }
+
+    vdom = view(state, emit);
+    mountDOM(vdom, parentEl);
+  }
+
+  return {
+    mount(_parentEl) {
+      parentEl = _parentEl;
+      renderApp();
+    },
+
+    unpount() {
+      destroyDOM(vdom);
+      vdom = null;
+      subscritions.forEach((unsubscribe) => unsubscribe());
+    },
+  };
+};
+
+module.exports = {
+  createApp,
+};
